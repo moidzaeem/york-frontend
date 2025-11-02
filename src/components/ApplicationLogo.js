605e0b72bfbd9c4bd99d4f8c67730a75b23e1ede
Faruk <faruk@Faruk-Air.bbrouter>
2025-11-02 03:15:12 +0300

    Destructive push of all files

--- src/components/ApplicationLogo.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image"
-
-const ApplicationLogo = props => (
-    
-    <Image src="/york-logo.png" width={155} height={58.79} alt="brand logo" className="w-[88px] h-[33px] sm:w-[155px] sm:h-[58.79px] aspect-video" />
-)
-
-export default ApplicationLogo
--- src/components/Label.js
+++
@@ -1,9 +0,0 @@
-const Label = ({ className, children, ...props }) => (
-    <label
-        className={`${className} flex items-center justify-start gap-2 font-medium text-base text-gray-700`}
-        {...props}>
-        {children}
-    </label>
-)
-
-export default Label
