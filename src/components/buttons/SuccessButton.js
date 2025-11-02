de495be47f021910f5752cfe8a524abab3bd4c10
Faruk <faruk@Faruk-Air.bbrouter>
2025-11-02 03:15:12 +0300

    Destructive push of all files

--- src/components/buttons/SuccessButton.js
+++
@@ -1,8 +0,0 @@
-
-const SuccessButton = ({ className = "", children }) => {
-    return (
-        <button className="text-nowrap btn btn-success">{ children }</button>
-    )
-}
-
-export default SuccessButton
